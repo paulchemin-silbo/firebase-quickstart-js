017856ac8abd7506327284bb3ff25413b7aac76e
paulchemin-silbo <paul.chemin@silbo.com>
2025-01-28 10:53:20 +0100

    build: remove base url

--- messaging/vite.config.js
+++ messaging/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/firebase-quickstart-js',
+  base: '/',
   build: {
     rollupOptions: {
       input: ['index.html', 'firebase-messaging-sw.js'],
